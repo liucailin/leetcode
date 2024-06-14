ef2d0ee1fa2373ed5d1efd6ddfa2b2213443dfa7
liu <liu@work.com>
2024-06-14 14:34:10 +0800

    80

---
+++ 80.remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=80 lang=python3
+#
+# [80] Remove Duplicates from Sorted Array II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+        duplicate = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k]:
+                k += 1
+                nums[k] = nums[i]
+                duplicate = 0
+            else:
+                duplicate += 1
+                if duplicate == 1:
+                    k += 1
+                    nums[k] = nums[i]
+        return k+1
+# @lc code=end
+
+def test(nums):
+    print(nums)
+    result = Solution().removeDuplicates(nums)
+    print(nums, result)
+
+# test([1,1,1,2,2,3])
+# test([0,0,1,1,1,1,2,3,3])
