023448215a7bde7725d9bb51f81cf0512d056a69
liucailin <liu1356@live.com>
2024-06-18 00:48:55 +0800

    160

--- 160.intersection-of-two-linked-lists.py
+++ 160.intersection-of-two-linked-lists.py
@@ -6,10 +6,13 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
