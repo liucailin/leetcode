41cdad638cc20005d9a6996bf3c1d4fc20c811d8
liu <liu@work.com>
2024-06-18 11:30:14 +0800

    1768

---
+++ 1768.merge-strings-alternately.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1768 lang=python3
+#
+# [1768] Merge Strings Alternately
+#
+
+# @lc code=start
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = j = k = 0
+        result = []
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
+            i += 1
+            j += 1
+
+        result.append(word1[i:])
+        result.append(word2[j:])
+
+        return ''.join(result)
+
+# @lc code=end
+
