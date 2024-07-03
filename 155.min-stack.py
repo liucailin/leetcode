0df0775eb44d34ac97645b7ea16617e39bad2722
liucailin <liu1356@live.com>
2024-07-03 21:10:51 +0800

    155

---
+++ 155.min-stack.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=155 lang=python3
+#
+# [155] Min Stack
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        # 用一个stack维护最小值，不用每次查询的时候循环
+        self.min_stack = []
+        
+
+    def push(self, val: int) -> None:
+
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            val = self.stack.pop()
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
+
