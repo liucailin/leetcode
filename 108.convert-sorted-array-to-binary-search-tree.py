03caec6a0ce77edfc335c16809c72e669c126389
liucailin <liu1356@live.com>
2024-07-04 21:27:01 +0800

    82

--- 108.convert-sorted-array-to-binary-search-tree.py
+++ 108.convert-sorted-array-to-binary-search-tree.py
@@ -10,11 +10,11 @@ from typing import List, Optional
 # from test import TreeNode
 
 
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
 
