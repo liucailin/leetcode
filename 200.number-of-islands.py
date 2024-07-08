55374dd2ae967f3e8ed9b7856e9792d51c8282f2
liucailin <liu1356@live.com>
2024-07-09 00:45:19 +0800

    200

---
+++ 200.number-of-islands.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def dfs(i, j):
+            if i < 0 or i >= len(grid):
+                return
+            if j < 0 or j >= len(grid[i]):
+                return
+            if grid[i][j] == '0':
+                return
+            grid[i][j] = '0'
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+
+
+        return count
+                
+        
+# @lc code=end
+
+print(Solution().numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]))
